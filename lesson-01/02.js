b264e9b8087df139765b5bd47cfde0db51870194
Ayanqq <nazbievayan16@gmail.com>
2023-12-09 14:43:19 +0600

    task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,6 @@
-// code
+let number1 = Math.floor(Math.random() * 100)
+if (number1 % 2 === 0) {
+    console.log(number1, ":Это число четное")
+} else {
+    console.log(number1, ":Это число нечетное")
+}
