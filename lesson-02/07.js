8071fb3c2124ff84d62af95b1cbb703d2e7dd3ed
Ayanqq <nazbievayan16@gmail.com>
2023-12-17 21:21:55 +0600

    task 6

---
+++ lesson-02/07.js
@@ -0,0 +1,21 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    },
+};
+
+let passportMarriedCopy = {
+    name: "Petr",
+    surname: "Petrov",
+    married: true,
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    },
+};
+
+console.log(passportMarried)
+console.log(passportMarriedCopy)
